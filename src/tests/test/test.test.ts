548254acb75f9fb2939d4820925c80e55a76468d
yared tsegaye <tsyared@gmail.com>
2023-01-21 09:53:16 +0300

    test: route not found test added

--- src/tests/test/test.test.ts
+++ src/tests/test/test.test.ts
@@ -2,10 +2,14 @@ import { StatusCodes } from 'http-status-codes';
 import supertest from 'supertest';
 import app from '@app';
 
-describe('first test', () => {
-    it('test', async () => {
-        const request = supertest(app);
+describe('Demo test', () => {
+    const request = supertest(app);
+    it('Success test', async () => {
         const response = await request.get('/test');
         expect(response.status).toBe(StatusCodes.OK);
+    });
+    it('Route Not Found test', async () => {
+        const response = await request.get('/');
+        expect(response.status).toBe(StatusCodes.NOT_FOUND);
     });
 });
