e68ccf238aa9b58d3278b7e85b61299b44157073
yared tsegaye <tsyared@gmail.com>
2023-03-11 16:27:13 +0300

    feat: local strategy added

--- src/helpers/passport.ts
+++ src/helpers/passport.ts
@@ -1,23 +1,52 @@
 import passport from 'passport';
-import { Strategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
-import { User, UserModel } from '@database/model/core';
+import {
+    Strategy as JWTStrategy,
+    ExtractJwt,
+    StrategyOptions
+} from 'passport-jwt';
+import { Strategy as LocalStrategy } from 'passport-local';
+//
+import {
+    findUserById,
+    findUserByEmail
+} from '@apps/Core/User/model/user.repository';
+import { SECRET_KEY } from '@config';
 
-const options: StrategyOptions = {
+const JWToptions: StrategyOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'TEMP'
+    secretOrKey: SECRET_KEY
 };
 
-const strategy = new Strategy(options, (payload, done) => {
-    UserModel.findOne({ _id: payload.sub })
-        .then((user: User | any) => {
-            if (user) {
-                return done(undefined, user);
-            } else {
-                return done(undefined, false);
-            }
-        })
-        .catch((err) => done(err, undefined));
+const strategy = new JWTStrategy(JWToptions, async (payload, done) => {
+    const user = await findUserById(payload.sub);
+
+    if (user) {
+        return done(undefined, { ...user, accessToken: payload.token });
+    } else {
+        return done(undefined, false);
+    }
 });
+
+passport.use(
+    'local',
+    new LocalStrategy(
+        { usernameField: 'email', passwordField: 'password' },
+        async (email, password, done) => {
+            const user = await findUserByEmail(email);
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username' });
+            }
+
+            const isCorrect = user!.validatePassword(password);
+            if (!isCorrect) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+
+            return done(null, user);
+        }
+    )
+);
+
 passport.serializeUser((user, done) => {
     done(null, user);
 });
